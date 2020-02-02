d8d73a83f150adba98882dc900ef61dd64adb919
Whitney Reid <whitreid@hotmail.com>
2020-02-02 13:50:45 -0500

    added images and description on page

--- mail.php
+++ mail.php
@@ -1,10 +1,16 @@
 <?php 
 
-	$to = ""; // this is your Email address
+	$to = "whitney.reid.codes@gmail.com"; // this is your Email address
 	$from  = $_POST['email']; // this is the sender's Email address
 	$sender_name = $_POST['name'];
-	$number_of_gustes = $_POST['guest'];
-	$events = $_POST['events'];
+  $attending = $_POST['attending'];
+	$guests = $_POST['guest'];
+  $menuGuestOne = $_POST['menuGuestOne'];
+  $menuGuestTwo = $_POST['menuGuestTwo'];
+  $allergies = $_POST['allergies'];
+  $drinking = $_POST['drinking'];
+  $drinkingAnswer = $_POST['drinkingAnswer'];
+  $hotel = $_POST['hotel'];
 	$notes = $_POST['notes'];
 
 
