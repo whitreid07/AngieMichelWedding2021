47fec5ab4dbc53a67f76ce469c07f4a674f6364e
Whitney Reid <49445634+whitreid07@users.noreply.github.com>
2022-08-02 16:39:33 -0400

    Updating countdown timer to wedding date!

--- js/script.js
+++ js/script.js
@@ -384,7 +384,7 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('4/5/11', function(event) {
+        $('#clock').countdown('2024/5/11', function(event) {
             var $this = $(this).html(event.strftime(''
             + '<div class="box"><div>%D</div> <span>Days</span> </div>'
             + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
