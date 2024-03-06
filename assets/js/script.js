13321993e7a11086a44f60fce84d77dcaefcf2fb
Whitney Reid <49445634+whitreid07@users.noreply.github.com>
2024-03-05 22:04:25 -0500

    Updating countdown timer May 9, 2026

--- assets/js/script.js
+++ assets/js/script.js
@@ -384,7 +384,7 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2024/5/11', function(event) {
+        $('#clock').countdown('2026/05/09', function(event) {
             var $this = $(this).html(event.strftime(''
             + '<div class="box"><div>%D</div> <span>Days</span> </div>'
             + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
